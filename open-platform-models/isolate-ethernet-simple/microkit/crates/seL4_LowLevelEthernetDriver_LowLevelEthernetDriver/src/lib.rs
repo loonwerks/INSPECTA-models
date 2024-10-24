17a0026722e7b47648dc9dfcf6b5805a73c430a8
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2024-10-24 10:50:19 -0400

    iso-eth-simple: Clean up comments

--- open-platform-models/isolate-ethernet-simple/microkit/crates/seL4_LowLevelEthernetDriver_LowLevelEthernetDriver/src/lib.rs
+++ open-platform-models/isolate-ethernet-simple/microkit/crates/seL4_LowLevelEthernetDriver_LowLevelEthernetDriver/src/lib.rs
@@ -1,35 +1,21 @@
-// use core::cell::RefCell;
-
-// use std::sync::{LazyLock, Mutex};
-// static VALUE: LazyLock<Mutex<Vec<u8>>> = LazyLock::new(|| Mutex::new(vec![]));
-
-// use core::cell::LazyCell;
-
-// static VALUE: LazyCell<Mutex<Vec<u8>>> = LazyCell::new(|| Mutex::new(vec![]));
-
-// #[no_mangle]
-// pub extern "C" fn seL4_LowLevelEthernetDriver_LowLevelEthernetDriver_initialize() {
-//     println!("Rust: Initialize");
-//     VALUE.lock().unwrap().push(1);
-// }
-
-// #[no_mangle]
-// pub extern "C" fn seL4_LowLevelEthernetDriver_LowLevelEthernetDriver_timeTriggered() {
-//     print!("Rust: ");
-//     {
-//         let mut v = VALUE.lock().unwrap();
-//         let last = v.last().unwrap().clone() + 1;
-//         println!("Updating value: {last}");
-//         v.push(last);
-//     }
-//     let val_ptr = VALUE.lock().unwrap().as_mut_ptr();
-//     unsafe {
-//         putEthernetFramesRx(val_ptr);
-//     }
-// }
 #![no_std]
 
 use sel4::debug_println;
+
+mod bindings {
+    pub const BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE: usize = 1600;
+    pub type BaseSwRawEthernetMessageImpl = [u8; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE];
+    extern "C" {
+        pub fn putEthernetFramesRx(value: *mut u8);
+    }
+    extern "C" {
+        pub fn getEthernetFramesTx(value: *mut u8);
+    }
+}
+use bindings::{
+    getEthernetFramesTx, putEthernetFramesRx, BaseSwRawEthernetMessageImpl,
+    BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE,
+};
 
 static mut VALUE: BaseSwRawEthernetMessageImpl = [0u8; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE];
 const DEFAULT_RX: u8 = 100;
@@ -59,56 +45,10 @@ pub extern "C" fn seL4_LowLevelEthernetDriver_LowLevelEthernetDriver_timeTrigger
     }
 }
 
-// #![feature(thread_local)]
-// #![feature(slice_pattern)]
-// use core::cell::RefCell;
-
-mod bindings {
-    pub const BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE: usize = 1600;
-    pub type BaseSwRawEthernetMessageImpl = [u8; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE];
-    extern "C" {
-        pub fn putEthernetFramesRx(value: *mut u8);
-    }
-    extern "C" {
-        pub fn getEthernetFramesTx(value: *mut u8);
-    }
-}
-use bindings::{
-    getEthernetFramesTx, putEthernetFramesRx, BaseSwRawEthernetMessageImpl,
-    BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE,
-};
-
-// #[thread_local]
-// static VALUE: RefCell<BaseSwRawEthernetMessageImpl> =
-//     RefCell::new([0; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE]);
-
-// #[no_mangle]
-// pub extern "C" fn seL4_LowLevelEthernetDriver_LowLevelEthernetDriver_initialize() {
-//     let mut rx = VALUE.borrow_mut();
-//     rx.iter_mut().for_each(|x| *x = DEFAULT_RX);
-// }
-
-// #[no_mangle]
-// pub extern "C" fn seL4_LowLevelEthernetDriver_LowLevelEthernetDriver_timeTriggered() {
-//     {
-//         let mut tx: BaseSwRawEthernetMessageImpl = [0; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE];
-//         unsafe { getEthernetFramesTx(tx.as_mut_ptr()) };
-//         let (int_bytes, _rest) = tx.split_at(core::mem::size_of::<u32>());
-//         let _val = u32::from_be_bytes(int_bytes.try_into().unwrap());
-//         // println!("TX Received: {val}");
-//     }
-//     {
-//         let mut ptr = VALUE.borrow_mut();
-//         let val = ptr.get_mut(0).unwrap();
-//         let new_val = if *val > 0 { *val - 1 } else { DEFAULT_RX };
-//         *val = new_val;
-//         unsafe { putEthernetFramesRx(ptr.as_mut_ptr()) };
-//         // println!("RX Sent {new_val}");
-//     }
-// }
-
+// Need a Panic handler in a no_std environment
 use core::panic::PanicInfo;
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    debug_println!("PANIC: {info:#?}");
     loop {}
 }
