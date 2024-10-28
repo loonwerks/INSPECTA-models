c358824cc829076afdb5db513a28232e485da773
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2024-10-28 16:25:29 -0400

    firewall: Finish implementing firewall
    Created a very basic network stack with just the pieces needed for the
    firewall, based on smoltcp.

--- open-platform-models/isolate-ethernet-simple/microkit/crates/seL4_Firewall_Firewall/src/config.rs
+++ open-platform-models/isolate-ethernet-simple/microkit/crates/seL4_Firewall_Firewall/src/config.rs
@@ -4,17 +4,10 @@
 // SPDX-License-Identifier: BSD-2-Clause
 //
 
-// pub mod channels {
-//     use sel4_microkit::Channel;
-
-//     pub const DEVICE: Channel = Channel::new(0);
-//     pub const CLIENT: Channel = Channel::new(1);
-// }
-
-// pub mod sizes {
-// pub const DRIVER_DMA: usize = 0x20_0000;
-// pub const NET_CLIENT_DMA: usize = 0x20_0000;
-// }
+pub mod net {
+    pub const MAC_ADDR: [u8; 6] = [0x00, 0x0A, 0x35, 0x03, 0x78, 0xA1];
+    pub const IPV4_ADDR: [u8; 4] = [192, 168, 60, 149];
+}
 
 pub mod log {
     use sel4::debug_print;
--- open-platform-models/isolate-ethernet-simple/microkit/crates/seL4_Firewall_Firewall/src/lib.rs
+++ open-platform-models/isolate-ethernet-simple/microkit/crates/seL4_Firewall_Firewall/src/lib.rs
@@ -1,17 +1,10 @@
 #![no_std]
 
-// use core::cell::OnceCell;
 use core::panic::PanicInfo;
-use log::{error, info};
-// use sel4::debug_println;
-// use sel4_driver_interfaces::HandleInterrupt;
-// use sel4_microkit_base::memory_region_symbol;
-// use smoltcp::{
-//     phy::{self, Device, RxToken, TxToken},
-//     time::Instant,
-// };
+use log::{debug, error, info, trace, warn};
 
 mod config;
+mod net;
 
 mod bindings {
     pub const BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE: usize = 1600;
@@ -28,95 +21,128 @@ use bindings::{
     BaseSwRawEthernetMessageImpl, BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE,
 };
 
+use net::{
+    Address, Arp, ArpOp, EtherType, EthernetRepr, IpProtocol, Ipv4Address, Ipv4Repr, TcpRepr,
+};
+
+fn filter(tcp: &TcpRepr) -> bool {
+    let allowed_ports = [5760u16];
+    !allowed_ports.iter().any(|x| *x == tcp.dst_port)
+}
+
 #[no_mangle]
 pub extern "C" fn seL4_Firewall_Firewall_initialize() {
     config::log::LOGGER.set().unwrap();
     info!("Init");
 }
 
-fn print_frame(pk: &BaseSwRawEthernetMessageImpl) {
-    info!("PACKET:\n {:?}", &pk[..64]);
-}
-
-#[derive(Debug)]
-pub struct Address(pub [u8; 6]);
-
-impl Address {
-    pub fn from_bytes(data: &[u8]) -> Address {
-        let mut bytes = [0; 6];
-        bytes.copy_from_slice(data);
-        Address(bytes)
-    }
-}
-
-#[derive(Debug)]
-#[repr(u16)]
-pub enum EtherType {
-    Ipv4 = 0x0800,
-    Arp = 0x0806,
-    Ipv6 = 0x86DD,
-    Unknown(u16),
-}
-
-impl From<u16> for EtherType {
-    fn from(value: u16) -> Self {
-        match value {
-            0x0800 => EtherType::Ipv4,
-            0x0806 => EtherType::Arp,
-            0x86DD => EtherType::Ipv6,
-            other => EtherType::Unknown(other),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct EthernetRepr {
-    pub src_addr: Address,
-    pub dst_addr: Address,
-    pub ethertype: EtherType,
-}
-
-impl EthernetRepr {
-    /// Parse an Ethernet II frame and return a high-level representation.
-    pub fn parse(frame: &BaseSwRawEthernetMessageImpl) -> EthernetRepr {
-        let dst_addr = Address::from_bytes(&frame[0..6]);
-        let src_addr = Address::from_bytes(&frame[6..12]);
-        let mut data = [0u8; 2];
-        data.copy_from_slice(&frame[12..14]);
-        let raw = u16::from_be_bytes(data);
-        let ethertype = EtherType::from(raw);
-        EthernetRepr {
-            src_addr,
-            dst_addr,
-            ethertype,
-        }
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.src_addr.0.iter().filter(|x| **x != 0).count() == 0
-    }
-}
-
 #[no_mangle]
 pub extern "C" fn seL4_Firewall_Firewall_timeTriggered() {
-    // info!("timeTriggered");
+    let mut tx_transmitted = false;
+    let mut rx_transmitted = false;
 
-    let mut rx: BaseSwRawEthernetMessageImpl = [0; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE];
-    unsafe { getEthernetFramesRxIn(rx.as_mut_ptr()) };
+    let mut frame: BaseSwRawEthernetMessageImpl = [0; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE];
 
-    let header = EthernetRepr::parse(&rx);
-    if !header.is_empty() {
-        info!("Dest Mac: {:?}", header.dst_addr);
-        info!("Source Mac: {:?}", header.src_addr);
-        info!("EtherType: {:?}", header.ethertype);
-        info!("PACKET:\n {:?}", &rx[14..64]);
+    // RX path
+    {
+        unsafe { getEthernetFramesRxIn(frame.as_mut_ptr()) };
+
+        let header = EthernetRepr::parse(&frame);
+        // TODO: Check for malformed Header
+
+        if !header.is_empty() {
+            debug!("Dest Mac: {:?}", header.dst_addr);
+            debug!("Source Mac: {:?}", header.src_addr);
+            debug!("EtherType: {:?}", header.ethertype);
+            match header.ethertype {
+                EtherType::Arp => {
+                    let arp = Arp::parse(&frame[EthernetRepr::SIZE..]);
+                    // TODO: Check for malformed ARP
+
+                    if arp.dest_protocol_addr.0 == config::net::IPV4_ADDR {
+                        debug!("ARP requested MY info");
+                        let arp_reply = Arp {
+                            htype: arp.htype,
+                            ptype: arp.ptype,
+                            hsize: arp.hsize,
+                            psize: arp.psize,
+                            op: ArpOp::Reply,
+                            src_addr: Address(config::net::MAC_ADDR),
+                            src_protocol_addr: Ipv4Address(config::net::IPV4_ADDR),
+                            dest_addr: arp.src_addr,
+                            dest_protocol_addr: arp.src_protocol_addr,
+                        };
+                        let header_reply = EthernetRepr {
+                            src_addr: Address(config::net::MAC_ADDR),
+                            dst_addr: header.src_addr,
+                            ethertype: EtherType::Arp,
+                        };
+                        // Not enough stack space for a new packet
+                        // let mut reply_packet: BaseSwRawEthernetMessageImpl =
+                        //     [0; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE];
+                        header_reply.emit(&mut frame[0..EthernetRepr::SIZE]);
+                        arp_reply
+                            .emit(&mut frame[EthernetRepr::SIZE..EthernetRepr::SIZE + Arp::SIZE]);
+
+                        unsafe { putEthernetFramesTxOut(frame.as_mut_ptr()) };
+                        tx_transmitted = true;
+                    }
+                }
+                EtherType::Ipv4 => {
+                    trace!("PACKET:\n {:?}", &frame[0..64]);
+                    let ip = Ipv4Repr::parse(&frame[EthernetRepr::SIZE..]);
+                    // TODO: Check that IPv4 Packet is not malformed
+
+                    match ip.protocol {
+                        IpProtocol::Tcp => {
+                            info!("TCP Packet, keep parsing");
+                            let tcp = TcpRepr::parse(&frame[EthernetRepr::SIZE + Ipv4Repr::SIZE..]);
+                            // TODO: Check that IPv4 Packet is not malformed
+
+                            if filter(&tcp) {
+                                info!("TCP packet filtered out");
+                            } else {
+                                unsafe { putEthernetFramesRxOut(frame.as_mut_ptr()) };
+                                rx_transmitted = true;
+                            }
+                        }
+                        // Throw away any packet that isn't IPv4
+                        _ => {
+                            info!("Not a TCP packet, throw away")
+                        }
+                    }
+                }
+                // Throw away any packet that isn't IPv4 or Arp
+                _ => {
+                    info!("Not an IPv4 or Arp packet, throw away")
+                }
+            }
+        }
     }
 
-    // TODO: Parse frameheader
-    // Check if there is a mac address
-    // if rx[0..6].iter().filter(|x| **x != 0).count() > 0 {
-    //     print_frame(&rx);
+    // TX Path
+    // {
+    //     unsafe { getEthernetFramesTxIn(frame.as_mut_ptr()) };
+    //     let header = EthernetRepr::parse(&frame);
+    //     // TODO: Check for malformed Header
+
+    //     if !header.is_empty() {
+    //         // TODO: Need to do any filtering this direction?
+
+    //         unsafe { putEthernetFramesTxOut(frame.as_mut_ptr()) };
+    //         tx_transmitted = true;
+    //     }
     // }
+
+    if !(rx_transmitted && tx_transmitted) {
+        frame.fill(0);
+        if !rx_transmitted {
+            unsafe { putEthernetFramesRxOut(frame.as_mut_ptr()) };
+        }
+        if !tx_transmitted {
+            unsafe { putEthernetFramesTxOut(frame.as_mut_ptr()) };
+        }
+    }
 }
 
 // Need a Panic handler in a no_std environment
