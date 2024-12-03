9c473d47a3149ffa75671eeeba047b7b8e39c811
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2024-12-03 12:06:21 -0500

    firewall: Allow ARP replys through firewall
    - The VMM will send out ARP requests, which means ARP replys need to go
    through the firewall.
    - Added loops around the API calls
    - Removed the status as it was not being used anymore

--- open-platform-models/isolate-ethernet-simple/microkit/crates/seL4_Firewall_Firewall/src/lib.rs
+++ open-platform-models/isolate-ethernet-simple/microkit/crates/seL4_Firewall_Firewall/src/lib.rs
@@ -5,7 +5,7 @@ use log::{debug, error, info, trace};
 
 mod config;
 mod net;
-mod status;
+// mod status;
 
 mod bindings {
     pub const BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE: usize = 1600;
@@ -33,7 +33,7 @@ use net::{
     Address, Arp, ArpOp, EtherType, EthernetRepr, IpProtocol, Ipv4Address, Ipv4Repr, TcpRepr,
 };
 
-use status::TransmitStatus;
+// use status::TransmitStatus;
 
 fn filter(tcp: &TcpRepr) -> bool {
     !config::firewall::tcp::ALLOWED_PORTS
@@ -47,154 +47,151 @@ pub extern "C" fn seL4_Firewall_Firewall_initialize() {
     info!("Init");
 }
 
-fn firewall_rx(frame: &mut BaseSwRawEthernetMessageImpl) -> TransmitStatus {
+fn firewall_rx(frame: &mut BaseSwRawEthernetMessageImpl) {
     // RX path
-    let mut status = TransmitStatus::new();
-    let new_data = unsafe { get_EthernetFramesRxIn(frame) };
-    if !new_data {
-        return status;
-    }
+    loop {
+        let new_data = unsafe { get_EthernetFramesRxIn(frame) };
+        if !new_data {
+            break;
+        }
 
-    let Some(header) = EthernetRepr::parse(frame) else {
-        return status;
-    };
+        let Some(header) = EthernetRepr::parse(frame) else {
+            continue;
+        };
 
-    // TODO: Do we still need this? Probably not because queuing tells us whether we have new data. However, is an all zero dest mac address a malformed packet?
-    if !header.is_empty() {
-        debug!("Dest Mac: {:?}", header.dst_addr);
-        debug!("Source Mac: {:?}", header.src_addr);
-        debug!("EtherType: {:?}", header.ethertype);
-        match header.ethertype {
-            EtherType::Arp => {
-                let Some(arp) = Arp::parse(&frame[EthernetRepr::SIZE..]) else {
-                    info!("Malformed ARP Packet. Throw it away.");
-                    return status;
-                };
-
-                if arp.dest_protocol_addr.0 == config::net::IPV4_ADDR {
-                    debug!("ARP requested MY info");
-                    let arp_reply = Arp {
-                        htype: arp.htype,
-                        ptype: arp.ptype,
-                        hsize: arp.hsize,
-                        psize: arp.psize,
-                        op: ArpOp::Reply,
-                        src_addr: Address(config::net::MAC_ADDR),
-                        src_protocol_addr: Ipv4Address(config::net::IPV4_ADDR),
-                        dest_addr: arp.src_addr,
-                        dest_protocol_addr: arp.src_protocol_addr,
+        // TODO: Do we still need this? Probably not because queuing tells us whether we have new data. However, is an all zero dest mac address a malformed packet?
+        if !header.is_empty() {
+            debug!("Dest Mac: {:?}", header.dst_addr);
+            debug!("Source Mac: {:?}", header.src_addr);
+            debug!("EtherType: {:?}", header.ethertype);
+            match header.ethertype {
+                EtherType::Arp => {
+                    let Some(arp) = Arp::parse(&frame[EthernetRepr::SIZE..]) else {
+                        info!("Malformed ARP Packet. Throw it away.");
+                        continue;
                     };
-                    let header_reply = EthernetRepr {
-                        src_addr: Address(config::net::MAC_ADDR),
-                        dst_addr: header.src_addr,
-                        ethertype: EtherType::Arp,
-                    };
-                    // Not enough stack space for a new packet
-                    // let mut reply_packet: BaseSwRawEthernetMessageImpl =
-                    //     [0; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE];
-                    header_reply.emit(&mut frame[0..EthernetRepr::SIZE]);
-                    arp_reply.emit(&mut frame[EthernetRepr::SIZE..EthernetRepr::SIZE + Arp::SIZE]);
 
-                    let mut out = BaseSwSizedEthernetMessageImpl {
-                        size: 64,
-                        message: *frame,
-                    };
-                    unsafe { put_EthernetFramesTxOut(&mut out) };
-                    status.tx = true;
-                }
-            }
-            EtherType::Ipv4 => {
-                trace!("PACKET:\n {:?}", &frame[0..64]);
-                let Some(ip) = Ipv4Repr::parse(&frame[EthernetRepr::SIZE..]) else {
-                    info!("Malformed IPv4 Packet. Throw it away.");
-                    return status;
-                };
-                // TODO: Check that the entire IPv4 Packet is not malformed
+                    match arp.op {
+                        ArpOp::Request => {
+                            if arp.dest_protocol_addr.0 == config::net::IPV4_ADDR {
+                                debug!("ARP requested MY info");
+                                let arp_reply = Arp {
+                                    htype: arp.htype,
+                                    ptype: arp.ptype,
+                                    hsize: arp.hsize,
+                                    psize: arp.psize,
+                                    op: ArpOp::Reply,
+                                    src_addr: Address(config::net::MAC_ADDR),
+                                    src_protocol_addr: Ipv4Address(config::net::IPV4_ADDR),
+                                    dest_addr: arp.src_addr,
+                                    dest_protocol_addr: arp.src_protocol_addr,
+                                };
+                                let header_reply = EthernetRepr {
+                                    src_addr: Address(config::net::MAC_ADDR),
+                                    dst_addr: header.src_addr,
+                                    ethertype: EtherType::Arp,
+                                };
+                                // Not enough stack space for a new packet
+                                // let mut reply_packet: BaseSwRawEthernetMessageImpl =
+                                //     [0; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE];
+                                header_reply.emit(&mut frame[0..EthernetRepr::SIZE]);
+                                arp_reply.emit(
+                                    &mut frame[EthernetRepr::SIZE..EthernetRepr::SIZE + Arp::SIZE],
+                                );
 
-                match ip.protocol {
-                    IpProtocol::Tcp => {
-                        let tcp = TcpRepr::parse(&frame[EthernetRepr::SIZE + Ipv4Repr::SIZE..]);
-                        // TODO: Check that TCP Packet is not malformed
-
-                        if filter(&tcp) {
-                            info!("TCP packet filtered out");
-                        } else {
-                            info!("Good TCP Packet. Send it along");
-                            unsafe { put_EthernetFramesRxOut(frame) };
-                            status.rx = true;
+                                let mut out = BaseSwSizedEthernetMessageImpl {
+                                    size: 64,
+                                    message: *frame,
+                                };
+                                unsafe { put_EthernetFramesTxOut(&mut out) };
+                            }
                         }
+                        ArpOp::Reply => {
+                            unsafe { put_EthernetFramesRxOut(frame) };
+                        }
+                        ArpOp::Unknown(_) => (),
                     }
-                    // Throw away any packet that isn't TCP
-                    _ => info!("Not a TCP packet. Throw it away."),
                 }
+                EtherType::Ipv4 => {
+                    trace!("PACKET:\n {:?}", &frame[0..64]);
+                    let Some(ip) = Ipv4Repr::parse(&frame[EthernetRepr::SIZE..]) else {
+                        info!("Malformed IPv4 Packet. Throw it away.");
+                        continue;
+                    };
+                    // TODO: Check that the entire IPv4 Packet is not malformed
+
+                    match ip.protocol {
+                        IpProtocol::Tcp => {
+                            let tcp = TcpRepr::parse(&frame[EthernetRepr::SIZE + Ipv4Repr::SIZE..]);
+                            // TODO: Check that TCP Packet is not malformed
+
+                            if filter(&tcp) {
+                                info!("TCP packet filtered out");
+                            } else {
+                                info!("Good TCP Packet. Send it along");
+                                unsafe { put_EthernetFramesRxOut(frame) };
+                            }
+                        }
+                        // Throw away any packet that isn't TCP
+                        _ => info!("Not a TCP packet. ({:?}) Throw it away.", ip.protocol),
+                    }
+                }
+                // Throw away any packet that isn't IPv4 or Arp
+                _ => info!("Not an IPv4 or Arp packet. Throw it away."),
             }
-            // Throw away any packet that isn't IPv4 or Arp
-            _ => info!("Not an IPv4 or Arp packet. Throw it away."),
         }
     }
-    status
 }
 
-fn firewall_tx(frame: &mut BaseSwRawEthernetMessageImpl) -> TransmitStatus {
-    let mut status = TransmitStatus::new();
-    let new_data = unsafe { get_EthernetFramesTxIn(frame) };
-    if !new_data {
-        return status;
-    }
+fn firewall_tx(frame: &mut BaseSwRawEthernetMessageImpl) {
+    loop {
+        let new_data = unsafe { get_EthernetFramesTxIn(frame) };
+        if !new_data {
+            break;
+        }
 
-    let Some(header) = EthernetRepr::parse(frame) else {
-        return status;
-    };
-
-    if !header.is_empty() {
-        // TODO: Need to do any filtering this direction?
-
-        let size = match header.ethertype {
-            EtherType::Arp => 64,
-            EtherType::Ipv4 => {
-                let Some(ip) = Ipv4Repr::parse(&frame[EthernetRepr::SIZE..]) else {
-                    info!("Malformed IPv4 Packet. Throw it away.");
-                    return status;
-                };
-                ip.length + EthernetRepr::SIZE as u16
-            }
-            // _ => EthernetRepr::SIZE as u16,
-            _ => {
-                info!("Not an IPv4 or Arp packet. Throw it away.");
-                return status;
-            }
+        let Some(header) = EthernetRepr::parse(frame) else {
+            continue;
         };
-        let mut out = BaseSwSizedEthernetMessageImpl {
-            size,
-            message: *frame,
-        };
-        unsafe { put_EthernetFramesTxOut(&mut out as *mut BaseSwSizedEthernetMessageImpl) };
-        status.tx = true;
+
+        if !header.is_empty() {
+            // TODO: Need to do any filtering this direction?
+
+            let size = match header.ethertype {
+                EtherType::Arp => {
+                    let size = 64u16;
+                    frame[EthernetRepr::SIZE + Arp::SIZE..size as usize].fill(0);
+                    size
+                }
+                EtherType::Ipv4 => {
+                    let Some(ip) = Ipv4Repr::parse(&frame[EthernetRepr::SIZE..]) else {
+                        info!("Malformed IPv4 Packet. Throw it away.");
+                        continue;
+                    };
+                    ip.length + EthernetRepr::SIZE as u16
+                }
+                // _ => EthernetRepr::SIZE as u16,
+                _ => {
+                    info!("Not an IPv4 or Arp packet. Throw it away.");
+                    continue;
+                }
+            };
+            let mut out = BaseSwSizedEthernetMessageImpl {
+                size,
+                message: *frame,
+            };
+            unsafe { put_EthernetFramesTxOut(&mut out as *mut BaseSwSizedEthernetMessageImpl) };
+        }
     }
-    status
 }
 
 #[no_mangle]
 pub extern "C" fn seL4_Firewall_Firewall_timeTriggered() {
-    info!("Triggered");
+    trace!("Triggered");
     let mut frame: BaseSwRawEthernetMessageImpl = [0; BASE_SW_RAWETHERNETMESSAGE_IMPL_SIZE];
 
-    let mut status = firewall_rx(&mut frame);
-    status |= firewall_tx(&mut frame);
-
-    // if status.any_untransmitted() {
-    //     frame.fill(0);
-    //     if !status.rx {
-    //         unsafe { put_EthernetFramesRxOut(frame.as_mut_ptr()) };
-    //     }
-    //     if !status.tx {
-    //         let mut out = BaseSwSizedEthernetMessageImpl {
-    //             size: 0,
-    //             message: frame,
-    //         };
-    //         unsafe { put_EthernetFramesTxOut(&mut out as *mut BaseSwSizedEthernetMessageImpl) };
-    //     }
-    // }
+    firewall_rx(&mut frame);
+    firewall_tx(&mut frame);
 }
 
 // Need a Panic handler in a no_std environment
