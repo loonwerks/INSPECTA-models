c3919982f316f6795e40a546a14c705fef487e5f
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2025-07-28 08:15:31 -0400

    txfirewall: Refine proptest strategies

--- open-platform-models/isolate-ethernet-simple/microkit/crates/seL4_TxFirewall_TxFirewall/src/bridge/test_api.rs
+++ open-platform-models/isolate-ethernet-simple/microkit/crates/seL4_TxFirewall_TxFirewall/src/bridge/test_api.rs
@@ -73,15 +73,150 @@ pub fn SW_RawEthernetMessage_strategy_default() -> impl Strategy<Value = SW::Raw
    SW_RawEthernetMessage_stategy_cust(any::<u8>())
  }
 
-pub fn SW_RawEthernetMessage_stategy_cust<u8_strategy: Strategy<Value = u8>> (base_strategy: u8_strategy) -> impl Strategy<Value = SW::RawEthernetMessage> 
- {
-   proptest::collection::vec(base_strategy, SW::SW_RawEthernetMessage_DIM_0)
-     .prop_map(|v| {
-       let boxed: Box<[u8; SW::SW_RawEthernetMessage_DIM_0]> = v.into_boxed_slice().try_into().unwrap();
-       *boxed
+// fn ethertype_strategy() -> impl Strategy<Value = u16> {
+//   prop_oneof![
+//      20 => Just(0x0800u16),
+//      10 => Just(0x0806u16),
+//      2 => Just(0x86DDu16),
+//      1 => any::<u16>(),
+//    ]
+// }
+ 
+fn ethertype_strategy() -> impl Strategy<Value = (u8, u8)> {
+  prop_oneof![
+     20 => Just((0x08u8, 0x00u8)),
+     10 => Just((0x08u8, 0x06u8)),
+     2 => Just((0x86u8, 0xDDu8)),
+     1 => (any::<u8>(),any::<u8>()),
+   ]
+}
+
+fn arp_ethertype_strategy() -> impl Strategy<Value = (u8, u8)> {
+  prop_oneof![
+     20 => Just((0x08u8, 0x00u8)),
+     2 => Just((0x08u8, 0x06u8)),
+     20 => Just((0x86u8, 0xDDu8)),
+     1 => (any::<u8>(),any::<u8>()),
+   ]
+}
+
+fn arp_hwtype_strategy() -> impl Strategy<Value = (u8, u8)> {
+  prop_oneof![
+     40 => Just((0x00u8, 0x01u8)),
+     1 => (any::<u8>(),any::<u8>()),
+   ]
+}
+
+fn arp_op_strategy() -> impl Strategy<Value = (u8, u8)> {
+  prop_oneof![
+     20 => Just((0x00u8, 0x01u8)),
+     20 => Just((0x00u8, 0x02u8)),
+     1 => (any::<u8>(),any::<u8>()),
+   ]
+}
+
+fn arp_strategy() -> impl Strategy<Value = Vec<u8>> {
+   arp_hwtype_strategy().prop_flat_map(move |hwtype| { 
+     arp_ethertype_strategy().prop_flat_map(move |ethertype| { 
+       arp_op_strategy().prop_flat_map(move |op| { 
+           proptest::collection::vec(any::<u8>(), 28)
+             .prop_map(move |mut v| {
+               v[0] = hwtype.0;
+               v[1] = hwtype.1;
+               v[2] = ethertype.0;
+               v[3] = ethertype.1;
+               v[6] = op.0;
+               v[7] = op.1;
+               v
+               })
+           })
+       })
    })
+}
+
+fn ipv4_protocol_strategy() -> impl Strategy<Value = u8> {
+  prop_oneof![
+      5 => Just(0x00), // HopByHop
+      5 => Just(0x01), // Icmp
+      5 => Just(0x02), // Igmp
+      5 => Just(0x06), // Tcp
+      5 => Just(0x11), // Udp
+      5 => Just(0x2b), // Ipv6Route
+      5 => Just(0x2c), // Ipv6Frag
+      5 => Just(0x3a), // Icmpv6
+      5 => Just(0x3b), // Ipv6NoNxt
+      5 => Just(0x3c), // Ipv6Opts
+      1 => any::<u8>(),
+   ]
+}
+
+fn ipv4_length_strategy() -> impl Strategy<Value = u16> {
+  prop_oneof![
+     40 => (0u16..=9000),
+     1 => (9001u16..),
+   ]
+}
+
+fn dst_mac_strategy() -> impl Strategy<Value = Vec<u8>> {
+  prop_oneof![
+     50 => proptest::collection::vec(any::<u8>(), 6),
+     1 => Just(vec![0,0,0,0,0,0]),
+   ]
+}
+
+fn ipv4_strategy() -> impl Strategy<Value = Vec<u8>> {
+   ipv4_length_strategy().prop_flat_map(move |length| { 
+     ipv4_protocol_strategy().prop_flat_map(move |proto| { 
+       proptest::collection::vec(any::<u8>(), 40)
+         .prop_map(move |mut v| {
+           // Better way to do this? maybe with a helper function
+           v[2] = (length >> 8) as u8;
+           v[3] = (length & 0xFF) as u8;
+           v[9] = proto;
+           v
+         })
+     })
+   })
+}
+
+fn default_packet_strategy() -> impl Strategy<Value = Vec<u8>> {
+  proptest::collection::vec(any::<u8>(), 1)
+}
+
+pub fn SW_RawEthernetMessage_stategy_cust<u8_strategy: Strategy<Value = u8> + Clone + Copy> (base_strategy: u8_strategy) -> impl Strategy<Value = SW::RawEthernetMessage> 
+ {
+   dst_mac_strategy().prop_flat_map(move |dst_mac| { 
+     ethertype_strategy().prop_flat_map(move |ethertype| { 
+       let packet = match ethertype {
+         (0x08u8, 0x00u8) => ipv4_strategy().boxed(),
+         (0x08u8, 0x06u8) => arp_strategy().boxed(),
+         _ => default_packet_strategy().boxed(),
+       };
+       packet.prop_flat_map({
+         let dst_mac = dst_mac.clone();
+         move |pack| {
+         proptest::collection::vec(base_strategy, SW::SW_RawEthernetMessage_DIM_0)
+           .prop_map({
+             let dst_mac = dst_mac.clone();
+             move |mut v| {
+             v.splice(0..6, dst_mac.iter().cloned());
+             v[12] = ethertype.0;
+             v[13] = ethertype.1;
+             v.splice(14..14+pack.len(), pack.iter().cloned());
+             // println!("{}", pack.len());
+             let boxed: Box<[u8; SW::SW_RawEthernetMessage_DIM_0]> = v.into_boxed_slice().try_into().unwrap();
+             *boxed
+         }})
+       }})
+    })
+  })
  }
 
+     // match ethertype {
+     //   0x0800 => todo!(),
+     //   0x0806 => todo!(),
+     //   _ => (),
+     // }
 pub fn SW_u16Array_strategy_default() -> impl Strategy<Value = SW::u16Array> 
  {
    SW_u16Array_stategy_cust(any::<u16>())
