abbafe3e09b8907a114e528908dddc8ef59f53e0
Jason Belt <belt@ksu.edu>
2026-01-16 12:06:26 -0600

    add codegen for sysml/array model

---
+++ micro-examples/microkit/gumbo-verus/structs_arrays/hamr/microkit_sysml/microkit/components/pacer/src/pacer.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <microkit.h>
+
+#define PORT_TO_PRODUCER_PRODUCER_MON 61
+#define PORT_TO_CONSUMER_CONSUMER_MON 59
+
+void init(void) {}
+
+void notified(microkit_channel channel) {
+  switch(channel) {
+    case PORT_TO_PRODUCER_PRODUCER_MON:
+      microkit_notify(PORT_TO_PRODUCER_PRODUCER_MON);
+      break;
+    case PORT_TO_CONSUMER_CONSUMER_MON:
+      microkit_notify(PORT_TO_CONSUMER_CONSUMER_MON);
+      break;
+  }
+}
