64361a42e10e0a775df3cf3a57a3f1a2997e5aa4
Alexis Aurandt <Alexis.Aurandt2@collins.com>
2026-08-20 11:44:40 -0400

    Run HAMR on Sysml examples with r2u2 monitors

---
+++ micro-examples/microkit/r2u2_monitor/r2u2_system_monitor/hamr/microkit_mcs_sysml/crates/monitor_process_monitor_thread/src/component/monitor_process_monitor_thread_app.rs
@@ -0,0 +1,83 @@
+// This file will not be overwritten if HAMR codegen is rerun
+
+use data::*;
+use crate::bridge::monitor_process_monitor_thread_api::*;
+use vstd::prelude::*;
+// BEGIN MARKER R2U2 MONITOR IMPORT
+use super::r2u2_monitor::*;
+// END MARKER R2U2 MONITOR IMPORT
+
+verus! {
+
+  pub struct monitor_process_monitor_thread {
+    // PLACEHOLDER MARKER STATE VARS
+    // BEGIN MARKER R2U2 MONITOR STATE VAR
+    pub r2u2_monitor: R2U2Monitor,
+    // END MARKER R2U2 MONITOR STATE VAR
+  }
+
+  impl monitor_process_monitor_thread {
+    pub fn new() -> Self
+    {
+      Self {
+        // PLACEHOLDER MARKER STATE VAR INIT
+        // BEGIN MARKER R2U2 MONITOR STATE VAR INIT
+        r2u2_monitor: default_r2u2_monitor(),
+        // END MARKER R2U2 MONITOR STATE VAR INIT
+      }
+    }
+
+    pub fn initialize<API: monitor_process_monitor_thread_Put_Api> (
+      &mut self,
+      api: &mut monitor_process_monitor_thread_Application_Api<API>)
+      ensures
+        // PLACEHOLDER MARKER INITIALIZATION ENSURES
+    {
+      // BEGIN MARKER R2U2 MONITOR INITIALIZE
+      load_spec(&mut self.r2u2_monitor);
+      // END MARKER R2U2 MONITOR INITIALIZE
+
+      log_info("initialize entrypoint invoked");
+    }
+
+    pub fn timeTriggered<API: monitor_process_monitor_thread_Full_Api> (
+      &mut self,
+      api: &mut monitor_process_monitor_thread_Application_Api<API>)
+      requires
+        // PLACEHOLDER MARKER TIME TRIGGERED REQUIRES
+      ensures
+        // PLACEHOLDER MARKER TIME TRIGGERED ENSURES
+    {
+      let sent_sample = api.get_sent_sample();
+      let observed_sample = api.get_observed_sample();
+      log_info("compute entrypoint invoked");
+    }
+
+    pub fn notify(
+      &mut self,
+      channel: microkit_channel)
+    {
+      // this method is called when the monitor does not handle the passed in channel
+      match channel {
+        _ => {
+          log_warn_channel(channel)
+        }
+      }
+    }
+  }
+
+  #[verifier::external_body]
+  pub fn log_info(msg: &str)
+  {
+    log::info!("{0}", msg);
+  }
+
+  #[verifier::external_body]
+  pub fn log_warn_channel(channel: u32)
+  {
+    log::warn!("Unexpected channel: {0}", channel);
+  }
+
+  // PLACEHOLDER MARKER GUMBO METHODS
+
+}
