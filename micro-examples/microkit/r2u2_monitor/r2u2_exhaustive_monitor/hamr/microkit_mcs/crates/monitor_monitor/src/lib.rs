991a86ce0c090b5db792fb2514ec66a3aa45023b
Alexis Aurandt <Alexis.Aurandt2@collins.com>
2026-08-20 15:59:44 -0400

    Rerun HAMR codegen

--- micro-examples/microkit/r2u2_monitor/r2u2_exhaustive_monitor/hamr/microkit_mcs/crates/monitor_monitor/src/lib.rs
+++ micro-examples/microkit/r2u2_monitor/r2u2_exhaustive_monitor/hamr/microkit_mcs/crates/monitor_monitor/src/lib.rs
@@ -56,7 +56,6 @@ pub extern "C" fn monitor_monitor_initialize() {
 pub extern "C" fn monitor_monitor_timeTriggered() {
   unsafe {
     if let Some(_app) = app.as_mut() {
-      // R2U2 observes inputs before the component computes and outputs afterward.
       _app.pre_timeTriggered(&compute_api);
       _app.timeTriggered(&mut compute_api);
       _app.post_timeTriggered(&mut compute_api);
